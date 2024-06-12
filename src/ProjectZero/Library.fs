75629afc5a876c0116a919fd115dfb2226abf4bc
sebastian.przyszlak <sebastian.przyszlak@ex7.pl>
2024-06-12 12:54:49 +0200

    Refactor fib with pattern matching

--- src/ProjectZero/Library.fs
+++ src/ProjectZero/Library.fs
@@ -1,8 +1,10 @@
 ﻿namespace ProjectZero
 
 module Calculator =
-    let add a b =
-        a + b
-        
+    let add a b = a + b
+
     let rec fib n =
-        if n < 2 then n else (fib (n - 1)) + (fib (n - 2))
+        match n with
+        | 0 -> 0
+        | 1 -> 1
+        | _ -> (fib (n - 1)) + (fib (n - 2))
