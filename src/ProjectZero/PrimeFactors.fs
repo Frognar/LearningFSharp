8d1945accfb4b376e0edd8eadfc4fac83931a728
sebastian.przyszlak <sebastian.przyszlak@ex7.pl>
2024-06-13 09:56:03 +0200

    Refactor factorsOf using pattern matching

--- src/ProjectZero/PrimeFactors.fs
+++ src/ProjectZero/PrimeFactors.fs
@@ -1,12 +1,9 @@
 module ProjectZero.PrimeFactors
-   
-let rec factorsOf (n : int) : List<int> =
-    if n > 1
-    then (
-        if n % 2 = 0
-        then List.concat [[ 2 ]; factorsOf (n / 2) ]
-        else (
-            if n % 3 = 0
-            then List.concat [[ 3 ]; factorsOf (n / 3) ]
-            else [n]))
+
+let rec factorsOf (n: int) : List<int> =
+    if n > 1 then
+        (match n with
+         | _ when n % 2 = 0 -> List.concat [ [ 2 ]; factorsOf (n / 2) ]
+         | _ when n % 3 = 0 -> List.concat [ [ 3 ]; factorsOf (n / 3) ]
+         | _ -> [ n ])
     else []
