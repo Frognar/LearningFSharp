baee8dd2ce13f8131da110ce341ea1dbf17dce98
sebastian.przyszlak <sebastian.przyszlak@ex7.pl>
2024-06-13 10:29:59 +0200

    strict check in Assert.Equivalent

--- tests/ProjectZero.Tests/PrimeFactorsTests.fs
+++ tests/ProjectZero.Tests/PrimeFactorsTests.fs
@@ -5,51 +5,51 @@ open Xunit
 
 [<Fact>]
 let ``Factors of 1 should be []`` () =
-    Assert.Equivalent([], factorsOf 1)
+    Assert.Equivalent([], factorsOf 1, true)
 
 [<Fact>]
 let ``Factors of 2 should be [ 2 ]`` () =
-    Assert.Equivalent([ 2 ], factorsOf 2)
+    Assert.Equivalent([ 2 ], factorsOf 2, true)
 
 [<Fact>]
 let ``Factors of 3 should be [ 3 ]`` () =
-    Assert.Equivalent([ 3 ], factorsOf 3)
+    Assert.Equivalent([ 3 ], factorsOf 3, true)
 
 [<Fact>]
 let ``Factors of 4 should be [ 2; 2 ]`` () =
-    Assert.Equivalent([ 2; 2 ], factorsOf 4)
+    Assert.Equivalent([ 2; 2 ], factorsOf 4, true)
 
 [<Fact>]
 let ``Factors of 5 should be [ 5 ]`` () =
-    Assert.Equivalent([ 5 ], factorsOf 5)
+    Assert.Equivalent([ 5 ], factorsOf 5, true)
 
 [<Fact>]
 let ``Factors of 6 should be [ 2; 3 ]`` () =
-    Assert.Equivalent([ 2; 3 ], factorsOf 6)
+    Assert.Equivalent([ 2; 3 ], factorsOf 6, true)
 
 [<Fact>]
 let ``Factors of 7 should be [ 7 ]`` () =
-    Assert.Equivalent([ 7 ], factorsOf 7)
+    Assert.Equivalent([ 7 ], factorsOf 7, true)
 
 [<Fact>]
 let ``Factors of 8 should be [ 2; 2; 2 ]`` () =
-    Assert.Equivalent([ 2; 2; 2 ], factorsOf 8)
+    Assert.Equivalent([ 2; 2; 2 ], factorsOf 8, true)
 
 [<Fact>]
 let ``Factors of 9 should be [ 3; 3 ]`` () =
-    Assert.Equivalent([ 3; 3 ], factorsOf 9)
+    Assert.Equivalent([ 3; 3 ], factorsOf 9, true)
 
 [<Fact>]
 let ``Factors of 10 should be [ 2; 5 ]`` () =
-    Assert.Equivalent([ 2; 5 ], factorsOf 10)
+    Assert.Equivalent([ 2; 5 ], factorsOf 10, true)
 
 [<Fact>]
 let ``Factors of 11 should be [ 11 ]`` () =
-    Assert.Equivalent([ 11 ], factorsOf 11)
+    Assert.Equivalent([ 11 ], factorsOf 11, true)
 
 [<Fact>]
 let ``Factors of 2 * 2 * 3 * 3 * 5 * 5 * 7 * 7 * 11 * 11 should be [ 2; 2; 3; 3; 5; 5; 7; 7; 11; 11 ]`` () =
-    Assert.Equivalent([ 2; 2; 3; 3; 5; 5; 7; 7; 11; 11 ], factorsOf (List.reduce (*) [ 2; 2; 3; 3; 5; 5; 7; 7; 11; 11 ]))
+    Assert.Equivalent([ 2; 2; 3; 3; 5; 5; 7; 7; 11; 11 ], factorsOf (List.reduce (*) [ 2; 2; 3; 3; 5; 5; 7; 7; 11; 11 ]), true)
 
 let intPow bas exp =
     [bas] |> List.replicate exp |> List.concat |> List.reduce (*)
@@ -63,4 +63,4 @@ let ``intPow a b should be a^b`` (a: int) b expected =
 
 [<Fact>]
 let ``Factors of 2^31 - 1 should be [ 2^31 - 1 ]`` () =
-    Assert.Equivalent([ (intPow 2 31) - 1 ], factorsOf ((intPow 2 31) - 1))
+    Assert.Equivalent([ (intPow 2 31) - 1 ], factorsOf ((intPow 2 31) - 1), true)
