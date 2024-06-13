c13c84a61ef045085f4e7947b0f6509f11ccd820
sebastian.przyszlak <sebastian.przyszlak@ex7.pl>
2024-06-13 09:53:19 +0200

    Calculate factors of 9

--- src/ProjectZero/PrimeFactors.fs
+++ src/ProjectZero/PrimeFactors.fs
@@ -5,5 +5,8 @@ let rec factorsOf (n : int) : List<int> =
     then (
         if n % 2 = 0
         then List.concat [[ 2 ]; factorsOf (n / 2) ]
-        else [n])
+        else (
+            if n % 3 = 0
+            then List.concat [[ 3 ]; factorsOf (n / 3) ]
+            else [n]))
     else []
