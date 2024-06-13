4896843cbd1aa19d46c3b1208fe92b6b19cdef50
sebastian.przyszlak <sebastian.przyszlak@ex7.pl>
2024-06-13 10:20:22 +0200

    Test for bigger numbers

--- tests/ProjectZero.Tests/PrimeFactorsTests.fs
+++ tests/ProjectZero.Tests/PrimeFactorsTests.fs
@@ -38,3 +38,29 @@ let ``Factors of 8 should be [ 2; 2; 2 ]`` () =
 [<Fact>]
 let ``Factors of 9 should be [ 3; 3 ]`` () =
     Assert.Equivalent([ 3; 3 ], factorsOf 9)
+
+[<Fact>]
+let ``Factors of 10 should be [ 2; 5 ]`` () =
+    Assert.Equivalent([ 2; 5 ], factorsOf 10)
+
+[<Fact>]
+let ``Factors of 11 should be [ 11 ]`` () =
+    Assert.Equivalent([ 11 ], factorsOf 11)
+
+[<Fact>]
+let ``Factors of 2 * 2 * 3 * 3 * 5 * 5 * 7 * 7 * 11 * 11 should be [ 2; 2; 3; 3; 5; 5; 7; 7; 11; 11 ]`` () =
+    Assert.Equivalent([ 2; 2; 3; 3; 5; 5; 7; 7; 11; 11 ], factorsOf (List.reduce (*) [ 2; 2; 3; 3; 5; 5; 7; 7; 11; 11 ]))
+
+let intPow bas exp =
+    [bas] |> List.replicate exp |> List.concat |> List.reduce (*)
+
+[<Theory>]
+[<InlineData(3, 2, 9)>]
+[<InlineData(2, 3, 8)>]
+[<InlineData(5, 5, 3125)>]
+let ``intPow a b should be a^b`` (a: int) b expected =
+    Assert.Equal(expected, intPow a b)
+
+[<Fact>]
+let ``Factors of 2^31 - 1 should be [ 2^31 - 1 ]`` () =
+    Assert.Equivalent([ (intPow 2 31) - 1 ], factorsOf ((intPow 2 31) - 1))
