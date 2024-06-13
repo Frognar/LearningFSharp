e7bde0cbab63808b0c4d85eaaf325f73ddef7589
sebastian.przyszlak <sebastian.przyszlak@ex7.pl>
2024-06-13 10:05:12 +0200

    Refactor factorsOf making it more general

--- src/ProjectZero/PrimeFactors.fs
+++ src/ProjectZero/PrimeFactors.fs
@@ -1,9 +1,10 @@
 module ProjectZero.PrimeFactors
 
-let rec factorsOf (n: int) : List<int> =
-    if n > 1 then
-        (match n with
-         | _ when n % 2 = 0 -> List.concat [ [ 2 ]; factorsOf (n / 2) ]
-         | _ when n % 3 = 0 -> List.concat [ [ 3 ]; factorsOf (n / 3) ]
-         | _ -> [ n ])
-    else []
+let factorsOf (n: int) : List<int> =
+    let rec loop n div res =
+        if n > 1 then (
+            if n % div = 0
+            then loop (n / div) div (List.concat [ [ div ]; res ])
+            else loop n (div + 1) res)
+        else res
+    loop n 2 []
