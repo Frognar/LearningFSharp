3f4c3b4165f18aa2537b3d99c64231d98d3378f2
sebastian.przyszlak <sebastian.przyszlak@ex7.pl>
2024-06-14 11:16:40 +0200

    Check balance in tests using checkBalance function

--- tests/KataTests/BankingSystemTests.fs
+++ tests/KataTests/BankingSystemTests.fs
@@ -7,20 +7,25 @@ open Kata.BankingSystem
 let ``Account can be created`` () =
     let account = Bank.createAccount "uniqueId" 1000
     Assert.Equal("uniqueId", account.Number)
-    Assert.Equal(1000, account.Balance)
+    
+[<Fact>]
+let ``Can check your balance`` () =
+    let account = Bank.createAccount "uniqueId" 1000
+    let balance = Bank.checkBalance account
+    Assert.Equal(1000, balance)
 
 [<Fact>]
 let ``Deposit should be added to account balance`` () =
     let account = Bank.createAccount "uniqueId" 1000
     let accountAfterDeposit = Bank.deposit account 100
-    Assert.Equal(1100, accountAfterDeposit.Balance)
+    Assert.Equal(1100, Bank.checkBalance accountAfterDeposit)
 
 [<Fact>]
 let ``Withdrawal should be removed from account balance`` () =
     let account = Bank.createAccount "uniqueId" 1000
     let accountAfterWithdrawal = Bank.withdrawal account 100
     match accountAfterWithdrawal with
-    | Some account -> Assert.Equal(900, account.Balance)
+    | Some account -> Assert.Equal(900, Bank.checkBalance account)
     | _ -> Assert.Fail()
 
 [<Fact>]
@@ -28,9 +33,3 @@ let ``Withdrawal should not be allowed when the balance int the account is less`
     let account = Bank.createAccount "uniqueId" 10
     let accountAfterWithdrawal = Bank.withdrawal account 100
     Assert.Equal(None, accountAfterWithdrawal)
-    
-[<Fact>]
-let ``Can check your balance`` () =
-    let account = Bank.createAccount "uniqueId" 1000
-    let balance = Bank.checkBalance account
-    Assert.Equal(1000, balance)
