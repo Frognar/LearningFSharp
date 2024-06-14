f2a439f814834d797647b4fbf618cf8cf33179c0
sebastian.przyszlak <sebastian.przyszlak@ex7.pl>
2024-06-14 08:51:15 +0200

    Add tests for factors of up to 8

--- tests/KataTests/Tests.fs
+++ tests/KataTests/Tests.fs
@@ -18,3 +18,19 @@ let ``Factors of 3 should be [ 3 ]`` () =
 [<Fact>]
 let ``Factors of 4 should be [ 2; 2 ]`` () =
     Assert.StrictEqual([ 2; 2 ], factorOf 4)
+
+[<Fact>]
+let ``Factors of 5 should be [ 5 ]`` () =
+    Assert.StrictEqual([ 5 ], factorOf 5)
+
+[<Fact>]
+let ``Factors of 6 should be [ 2; 3 ]`` () =
+    Assert.StrictEqual([ 2; 3 ], factorOf 6)
+
+[<Fact>]
+let ``Factors of 7 should be [ 7 ]`` () =
+    Assert.StrictEqual([ 7 ], factorOf 7)
+
+[<Fact>]
+let ``Factors of 8 should be [ 2; 2; 2 ]`` () =
+    Assert.StrictEqual([ 2; 2; 2 ], factorOf 8)
