ddeea6b8865be66835323129f7934070a1e898a3
sebastian.przyszlak <sebastian.przyszlak@ex7.pl>
2024-06-14 08:59:58 +0200

    Test more numbers

--- tests/KataTests/Tests.fs
+++ tests/KataTests/Tests.fs
@@ -38,3 +38,31 @@ let ``Factors of 8 should be [ 2; 2; 2 ]`` () =
 [<Fact>]
 let ``Factors of 9 should be [ 3; 3 ]`` () =
     Assert.StrictEqual([ 3; 3 ], factorOf 9)
+
+[<Fact>]
+let ``Factors of 10 should be [ 2; 5 ]`` () =
+    Assert.StrictEqual([ 2; 5 ], factorOf 10)
+
+[<Fact>]
+let ``Factors of 11 should be [ 11 ]`` () =
+    Assert.StrictEqual([ 11 ], factorOf 11)
+
+[<Fact>]
+let ``Factors of 12 should be [ 2; 2; 3 ]`` () =
+    Assert.StrictEqual([ 2; 2; 3 ], factorOf 12)
+
+[<Fact>]
+let ``Factors of 13 should be [ 13 ]`` () =
+    Assert.StrictEqual([ 13 ], factorOf 13)
+
+[<Fact>]
+let ``Factors of 14 should be [ 2; 7 ]`` () =
+    Assert.StrictEqual([ 2; 7 ], factorOf 14)
+
+[<Fact>]
+let ``Factors of 15 should be [ 3; 5 ]`` () =
+    Assert.StrictEqual([ 3; 5 ], factorOf 15)
+
+[<Fact>]
+let ``Factors of 2 * 2 * 3 * 3 * 5 * 5 * 7 * 7 * 11 * 11 * 13 * 13 should be [ 2; 2; 3; 3; 5; 5; 7; 7; 11; 11; 13; 13 ]`` () =
+    Assert.StrictEqual([ 2; 2; 3; 3; 5; 5; 7; 7; 11; 11; 13; 13 ], factorOf (2 * 2 * 3 * 3 * 5 * 5 * 7 * 7 * 11 * 11 * 13 * 13))
