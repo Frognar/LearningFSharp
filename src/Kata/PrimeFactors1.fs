9ea1ad2bf8dd96e08e10e113708c6de7d8ff3b2a
frognar <sebastian.przyszlak@gmail.com>
2024-06-15 17:55:25 +0200

    Calculate factors of 9

--- src/Kata/PrimeFactors1.fs
+++ src/Kata/PrimeFactors1.fs
@@ -4,4 +4,5 @@ let rec factorsOf n =
     match n with
     | _ when n < 2 -> []
     | _ when n % 2 = 0 -> 2 :: factorsOf (n / 2)
+    | _ when n % 3 = 0 -> 3 :: factorsOf (n / 3)
     | _ -> [ n ]
--- tests/KataTests/PrimeFactors1Tests.fs
+++ tests/KataTests/PrimeFactors1Tests.fs
@@ -18,3 +18,23 @@ let ``Factors of 3 should be [ 3 ]`` () =
 [<Fact>]
 let ``Factors of 4 should be [ 2; 2 ]`` () =
     Assert.StrictEqual([ 2; 2 ], factorsOf 4)
+
+[<Fact>]
+let ``Factors of 5 should be [ 5 ]`` () =
+    Assert.StrictEqual([ 5 ], factorsOf 5)
+
+[<Fact>]
+let ``Factors of 6 should be [ 2; 3 ]`` () =
+    Assert.StrictEqual([ 2; 3 ], factorsOf 6)
+
+[<Fact>]
+let ``Factors of 7 should be [ 7 ]`` () =
+    Assert.StrictEqual([ 7 ], factorsOf 7)
+
+[<Fact>]
+let ``Factors of 8 should be [ 2; 2; 2 ]`` () =
+    Assert.StrictEqual([ 2; 2; 2 ], factorsOf 8)
+
+[<Fact>]
+let ``Factors of 9 should be [ 3; 3 ]`` () =
+    Assert.StrictEqual([ 3; 3 ], factorsOf 9)
