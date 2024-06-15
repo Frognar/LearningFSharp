9a2e64e6a3974e3b46e15de9da0823d247712ab2
frognar <sebastian.przyszlak@gmail.com>
2024-06-15 18:03:26 +0200

    primes up to 2 = [ 2 ]

--- src/Kata/Primes.fs
+++ src/Kata/Primes.fs
@@ -1,4 +1,4 @@
 module Kata.Primes
 
 let primesUpTo n =
-    []
+    if n < 2 then [] else [ 2 ]
--- tests/KataTests/PrimeTests.fs
+++ tests/KataTests/PrimeTests.fs
@@ -6,3 +6,7 @@ open Kata.Primes
 [<Fact>]
 let ``Primes up to 1 should be []`` () =
     Assert.StrictEqual([], primesUpTo 1)
+
+[<Fact>]
+let ``Primes up to 2 should be [ 2 ]`` () =
+    Assert.StrictEqual([ 2 ], primesUpTo 2)
