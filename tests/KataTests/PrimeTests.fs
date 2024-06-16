a8385e616cca86cfe6d5940b2a228b9dd7953031
frognar <sebastian.przyszlak@gmail.com>
2024-06-16 06:56:27 +0200

    Test for primes up to 100

--- tests/KataTests/PrimeTests.fs
+++ tests/KataTests/PrimeTests.fs
@@ -15,9 +15,20 @@ let ``Primes up to 2 should be [ 2 ]`` () =
 let ``Primes up to 3 should be [ 2; 3 ]`` () =
     Assert.StrictEqual([ 2; 3 ], primesUpTo 3)
 
-//[<Fact>]
-//let ``Primes up to 4 should be [ 2; 3 ]`` () =
-//    Assert.StrictEqual([ 2; 3 ], primesUpTo 4)
+[<Fact>]
+let ``Primes up to 4 should be [ 2; 3 ]`` () =
+    Assert.StrictEqual([ 2; 3 ], primesUpTo 4)
+
+[<Fact>]
+let ``Primes up to 5 should be [ 2; 3; 5 ]`` () =
+    Assert.StrictEqual([ 2; 3; 5 ], primesUpTo 5)
+
+[<Fact>]
+let ``Primes up to 15 should be [ 2; 3; 5; 7; 11; 13 ]`` () =
+    Assert.StrictEqual([ 2; 3; 5; 7; 11; 13 ], primesUpTo 15)
+[<Fact>]
+let ``Primes up to 100 should be [ 2; 3; 5; 7; 11; 13; 17; 19; 23; 29; 31; 37; 41; 43; 47; 53; 59; 61; 67; 71; 73; 79; 83; 89; 97 ]`` () =
+    Assert.StrictEqual([ 2; 3; 5; 7; 11; 13; 17; 19; 23; 29; 31; 37; 41; 43; 47; 53; 59; 61; 67; 71; 73; 79; 83; 89; 97 ], primesUpTo 100)
 
 [<Fact>]
 let ``Remove duplicates of first [ 2 ] should be []`` () =
