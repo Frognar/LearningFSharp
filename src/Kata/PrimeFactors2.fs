c64728166ae7446a6e62dfc17d54b118d5840802
sebastian.przyszlak <sebastian.przyszlak@ex7.pl>
2024-06-17 09:19:22 +0200

    factorsOf 3

--- src/Kata/PrimeFactors2.fs
+++ src/Kata/PrimeFactors2.fs
@@ -3,4 +3,4 @@ module Kata.PrimeFactors2
 let factorsOf n =
     match n with
     | _ when n < 2 -> []
-    | _ -> [ 2 ]
+    | _ -> [ n ]
--- tests/KataTests/PrimeFactors2Tests.fs
+++ tests/KataTests/PrimeFactors2Tests.fs
@@ -10,3 +10,7 @@ let ``Factors of 1 should be []`` () =
 [<Fact>]
 let ``Factors of 2 should be [ 2 ]`` () =
     Assert.StrictEqual([ 2 ], factorsOf 2)
+
+[<Fact>]
+let ``Factors of 3 should be [ 3 ]`` () =
+    Assert.StrictEqual([ 3 ], factorsOf 3)
