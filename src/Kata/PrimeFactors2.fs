7b8b23b1793df5b4fad5607291c308ad1ac0e7cd
sebastian.przyszlak <sebastian.przyszlak@ex7.pl>
2024-06-17 09:23:42 +0200

    Refactor factorsOf

--- src/Kata/PrimeFactors2.fs
+++ src/Kata/PrimeFactors2.fs
@@ -1,8 +1,10 @@
 module Kata.PrimeFactors2
 
-let rec factorsOf n =
-    match n with
-    | _ when n < 2 -> []
-    | _ when n % 2 = 0 -> 2 :: factorsOf (n / 2)
-    | _ when n % 3 = 0 -> 3 :: factorsOf (n / 3)
-    | _ -> [ n ]
+let factorsOf n =
+    let rec factorize x div factors =
+        match x with
+        | _ when x < 2 -> factors |> List.rev
+        | _ when x % div = 0 -> factorize (x / div) div (div :: factors)
+        | _ -> factorize x (div + 1) factors
+    
+    factorize n 2 []
