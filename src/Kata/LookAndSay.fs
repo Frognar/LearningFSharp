a9f19ff9458233e968a27b9d09d21e2b50acd48f
frognar <sebastian.przyszlak@gmail.com>
2024-06-18 20:21:57 +0200

    Refactor lookAndSay with pattern matching and active patterns

--- src/Kata/LookAndSay.fs
+++ src/Kata/LookAndSay.fs
@@ -1,16 +1,18 @@
 module Kata.LookAndSay
 
+let (|FirstTwoEquals|_|) (s: string) =
+    if s[0] = s[1] then Some()
+    else None
+
+let (|SingleChar|_|) s = if String.length s = 1 then Some() else None    
+
 let lookAndSay n =
-    if String.length n > 1 then
+    if String.length n = 1 then "1" + n
+    else
         let rec loop result count (input: string) =
-            if String.length input > 1 then
-                if input[0] = input[1] then
-                    loop result (count + 1) input[1..]
-                else
-                    loop (result + (string count + string input[0])) 1 input[1..]
-            else
-                result + (string count + input)
+            match input with
+            | SingleChar -> result + (string count + input)
+            | FirstTwoEquals -> loop result (count + 1) input[1..]
+            | _ -> loop (result + (string count + string input[0])) 1 input[1..]
                 
         loop "" 1 n
-    else
-        "1" + n
