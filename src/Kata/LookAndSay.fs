ff78ce85f8a42c760a5e579420dc858ca6a393c1
frognar <sebastian.przyszlak@gmail.com>
2024-06-18 19:46:37 +0200

    refactor 21 -> 1211

--- src/Kata/LookAndSay.fs
+++ src/Kata/LookAndSay.fs
@@ -2,8 +2,9 @@ module Kata.LookAndSay
 
 let lookAndSay n =
     if String.length n > 1 then
-        let current = n[0]
-        let next = n[1]
+        let index = 0
+        let current = n[index]
+        let next = n[index + 1]
         if current = next then "2" + string current
         else "1" + string current + "1" + string next
     else
