a988c8eb8982105484b643722472953e57738c4a
sebastian.przyszlak <sebastian.przyszlak@ex7.pl>
2024-06-18 08:10:49 +0200

    Refactor Render with Active patterns

--- src/Kata/FizzBuzz0.fs
+++ src/Kata/FizzBuzz0.fs
@@ -1,8 +1,18 @@
 module Kata.FizzBuzz0
 
+let (|MultOf3|_|) i =
+    match i with
+    | _ when i % 3 = 0 -> Some MultOf3
+    | _ -> None
+    
+let (|MultOf5|_|) i =
+    match i with
+    | _ when i % 5 = 0 -> Some MultOf5
+    | _ -> None
+
 let render n =
     match n with
-    | _ when n % 3 = 0 && n % 5 = 0 -> "FizzBuzz"
-    | _ when n % 3 = 0 -> "Fizz"
-    | _ when n % 5 = 0 -> "Buzz"
+    | MultOf3 & MultOf5 -> "FizzBuzz"
+    | MultOf3 -> "Fizz"
+    | MultOf5 -> "Buzz"
     | _ -> string n
