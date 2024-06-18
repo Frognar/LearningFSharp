5e278e6c4fc7971ec6e18a0bd7428d7db4612a80
frognar <sebastian.przyszlak@gmail.com>
2024-06-18 19:40:34 +0200

    21 -> 1211

--- src/Kata/LookAndSay.fs
+++ src/Kata/LookAndSay.fs
@@ -1,5 +1,6 @@
 module Kata.LookAndSay
 
 let lookAndSay n =
-    if String.length n = 1 then (string 1) + n
-    else (string 2) + (string n[0])
+    if String.length n = 1 then "1" + n
+    else if n[0] = n[1] then "2" + (string n[0])
+    else "1" + string n[0] + "1" + string n[1]
--- tests/KataTests/LookAndSayTests.fs
+++ tests/KataTests/LookAndSayTests.fs
@@ -6,5 +6,6 @@ open Kata.LookAndSay
 [<Theory>]
 [<InlineData("1", "11")>]
 [<InlineData("11", "21")>]
+[<InlineData("21", "1211")>]
 let ``Look at X and say Y`` x expected =
     Assert.Equal(expected, lookAndSay x)
