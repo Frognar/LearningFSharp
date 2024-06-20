d61a29a7754ca4ba984de2e6ef94ebb907fd1ed7
frognar <sebastian.przyszlak@gmail.com>
2024-06-20 15:19:21 +0200

    Refactor -> remove honorifics after split

--- src/Kata/NameInverter.fs
+++ src/Kata/NameInverter.fs
@@ -6,12 +6,16 @@ let isHonorific (candidate: string) =
     let knownHonorifics = [ "Mr." ]
     List.contains candidate knownHonorifics
 
+let withoutHonorifics nameParts =
+    match nameParts with
+    | candidate :: rest when isHonorific candidate -> rest
+    | _ -> nameParts
+
 let invert (name: string) =
-    match name.Split(' ', StringSplitOptions.RemoveEmptyEntries) |> Array.toList with
+    let nameParts = name.Split(' ', StringSplitOptions.RemoveEmptyEntries) |> Array.toList
+    match nameParts |> withoutHonorifics with
     | [ first ] -> first
     | [ first; last ] -> last + ", " + first
-    | [ honorific; first; last ] when isHonorific honorific -> last + ", " + first
-    | honorific :: first :: last :: tail when isHonorific honorific -> tail |> (List.fold (fun s v -> s + " " + v) (last + ", " + first))
     | first :: last :: tail -> tail |> (List.fold (fun s v -> s + " " + v) (last + ", " + first))
     | _ -> ""
     
