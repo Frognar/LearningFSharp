640b1d27e71372a5f406ede143ded92258a78df9
frognar <sebastian.przyszlak@gmail.com>
2024-06-23 16:26:49 +0200

    Refactor with Active patterns

--- src/Kata/LookAndSay1.fs
+++ src/Kata/LookAndSay1.fs
@@ -1,11 +1,27 @@
 module Kata.LookAndSay1
 
+let (|Empty|_|) x =
+    match x with
+    | "" -> Some ()
+    | _ -> None
+
+let (|SingleChar|_|) x =
+    match String.length x with
+    | 1 -> Some x
+    | _ -> None
+
+let (|FirstTwoEquals|_|) x =
+    match x with
+    | Empty | SingleChar _ -> None
+    | _ when x[0] = x[1] -> Some ()
+    | _ -> None
+
 let lookAndSay number =
     let rec loop x count result =
-        match String.length x with
-        | 0 -> ""
-        | 1 -> result + string count + x
-        | _ when x[0] = x[1] -> loop x[1..] (count + 1) result
+        match x with
+        | Empty -> ""
+        | SingleChar c -> result + string count + c
+        | FirstTwoEquals -> loop x[1..] (count + 1) result
         | _ -> loop x[1..] 1 (result + string count + string x[0])
     
     loop number 1 ""
