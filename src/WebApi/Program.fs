b4b521ac76d14bd8ebfbd7e7ed24fdd18eec6525
frognar <sebastian.przyszlak@gmail.com>
2024-06-26 22:10:42 +0200

    Register context

--- src/WebApi/Program.fs
+++ src/WebApi/Program.fs
@@ -13,6 +13,7 @@ open Microsoft.Extensions.Configuration
 open Microsoft.Extensions.DependencyInjection
 open Microsoft.Extensions.Hosting
 open Microsoft.Extensions.Logging
+open WebApi.Data
 
 module Program =
     let exitCode = 0
@@ -23,6 +24,7 @@ module Program =
         let builder = WebApplication.CreateBuilder(args)
 
         builder.Services.AddControllers()
+        builder.Services.AddDbContext<HabitContext>()
 
         let app = builder.Build()
 
