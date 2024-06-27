36fc1011000ac384195508b2efdb7a4f38185ee0
frognar <sebastian.przyszlak@gmail.com>
2024-06-27 16:34:51 +0200

    Save habit from POST method if correct

--- src/WebApi/Habits/HabitsController.fs
+++ src/WebApi/Habits/HabitsController.fs
@@ -1,10 +1,11 @@
 namespace WebApi.Habits
 
 open Microsoft.AspNetCore.Mvc
+open WebApi.Data
 
 [<ApiController>]
 [<Route("habits")>]
-type HabitsController() =
+type HabitsController(context: HabitContext) =
     inherit ControllerBase()
     
     
@@ -28,6 +29,14 @@ type HabitsController() =
     member this.Post([<FromBody>] habitDto: {|Name : string; Frequency: string|}) =
         let frequency = Parser.parseFrequency habitDto.Frequency
         let habit = frequency |> Option.map (fun f -> { Id = System.Guid.NewGuid(); Name = habitDto.Name; Frequency = f }: Habit)
-        match habit with
-        | Some x -> printf $"{x}"
-        | None -> printf "Wrong frequency!"
+        let dbHabit = habit |> Option.map (fun h -> { Id = h.Id; Name = h.Name; Frequency = h.Frequency.ToString() }: DbHabit)
+        let saveHabit (c: HabitContext) (h: DbHabit) =
+            c.Habits.Add(h)
+            c.SaveChanges() |> ignore
+            h
+            
+        let savedHabit = dbHabit |> Option.map (saveHabit context)
+        
+        match savedHabit with
+        | Some x -> this.Ok x :> IActionResult
+        | None -> this.BadRequest "Wrong frequency!" :> IActionResult
