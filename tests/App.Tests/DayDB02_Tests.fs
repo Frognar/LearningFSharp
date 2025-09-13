44f8a189a212e0aff6884e1390fb818e3fb6305c
frognar <sebastian.przyszlak@gmail.com>
2025-09-13 06:33:42 +0200

    add DayDB02_Tests with basic PostgreSQL fixture and connection setup

---
+++ tests/App.Tests/DayDB02_Tests.fs
@@ -0,0 +1,41 @@
+module App.Tests.DayDB02_Tests
+
+open System
+open System.Data
+open System.Threading.Tasks
+open App.Domain
+open Dapper
+open Npgsql
+open Testcontainers.PostgreSql
+open Xunit
+
+type PgFixture() =
+    let container =
+        PostgreSqlBuilder()
+            .WithImage("postgres:16-alpine")
+            .WithDatabase("testdb")
+            .WithUsername("test")
+            .WithPassword("test")
+            .Build()
+
+    member _.ConnStr = container.GetConnectionString()
+
+    interface IAsyncLifetime with
+      member _.InitializeAsync() =
+        task {
+          do! container.StartAsync()
+          do! Migrations.run (container.GetConnectionString())
+        } :> Task
+
+      member _.DisposeAsync() =
+        task {
+          do! container.DisposeAsync().AsTask()
+        } :> Task
+        
+type DbTests(fx: PgFixture) =
+    interface IClassFixture<PgFixture>
+
+    member private _.openConn() =
+        let c = new NpgsqlConnection(fx.ConnStr)
+        c.Open()
+        c :> IDbConnection
